8eed73d07bb91e84d094e0c3f5e8bb4f41475601
Noel An <noel.an@hotmail.fr>
2020-01-09 10:53:58 +0100

    hello

--- src/App.js
+++ src/App.js
@@ -3,19 +3,6 @@ import "./App.css";
 import Carousel from "./components/carousel";
 
 function App() {
-  const path = require("path");
-  const express = require("express");
-  const app = express();
-  const publicPath = path.join(__dirname, "..", "public");
-  const port = process.env.PORT || 3000;
-  app.use(express.static(publicPath));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(publicPath, "index.html"));
-  });
-  app.listen(port, () => {
-    console.log("Server is up!");
-  });
-
   return (
     <div className="App">
       <Carousel></Carousel>
