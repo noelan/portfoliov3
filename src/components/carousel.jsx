cd86c4fbf9fbfe3f078e8f3c454794a2523e9058
Noel An <noel.an@hotmail.fr>
2020-01-10 05:07:13 +0100

    ok

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -44,7 +44,7 @@ const Carousel = props => {
 
   return (
     <>
-      <div className="hideSm">
+      <div>
         {(loading && (
           <div className="loader" onLoad={() => console.log("LOADED")}>
             <Loader
@@ -57,134 +57,167 @@ const Carousel = props => {
           </div>
         )) || (
           <>
-            <div className="contactMe ">
-              <p>
-                <i class="fas fa-envelope"></i>noel.an.lyon@gmail.com
-              </p>
-              <p>
-                <i class="fas fa-phone"></i>06 83 07 54 48
-              </p>
-              <a href="https://www.linkedin.com/in/noel-an-704482186/">
-                <p>
-                  <i class="fab fa-linkedin"></i>Linkedin
-                </p>
-              </a>
-            </div>
-            <Fade>
-              <Slider {...settings}>
-                {/* Slide 1 */}
-                <div className="carouselSlide myBlack">
-                  <Slide top>
-                    <h3 className="myTitle text-center">Noël AN</h3>
-                  </Slide>
-                  <Fade right>
-                    <p className="underTitle">Je suis développeur web</p>
-                  </Fade>
-                </div>
-                {/* End Slide 1 */}
-                {/* Slide 2 */}
-                <div className="carouselSlide  myGrey">
-                  <div className={hide1 ? "hide" : ""}>
+            <div className="hideSm ">
+              <Fade>
+                <Slider {...settings}>
+                  {/* Slide 1 */}
+
+                  <div className="carouselSlide myBlack">
+                    <div className="contactMe">
+                      <p>
+                        <i className="fas fa-envelope"></i>
+                        noel.an.lyon@gmail.com
+                      </p>
+                      <p>
+                        <i className="fas fa-phone"></i>06 83 07 54 48
+                      </p>
+                      <a href="https://www.linkedin.com/in/noel-an-704482186/">
+                        <p>
+                          <i className="fab fa-linkedin"></i>Linkedin
+                        </p>
+                      </a>
+                    </div>
                     <Slide top>
-                      <h3 className="myTitleAboutMe">About me</h3>
+                      <h3 className="myTitle text-center">Noël AN</h3>
                     </Slide>
                     <Fade right>
-                      <div className="textAboutMe test">
-                        <p>
-                          Bonjour je me présente Noël AN. Après avoir travaillé
-                          principalement dans la restauration et la logistique,
-                          j'ai décidé de me reconvertir dans un domaine qui m'a
-                          toujours passionné, le devéloppement Web.
-                        </p>
-                        <p>
-                          J'ai effectué une formation de développeur web à la
-                          Wild Code School qui m'a permis de découvrir ce métier
-                          et me spécialiser en PHP Symfony.
-                        </p>
-                        <p>
-                          Je continue de m'auto-former continuellement,
-                          récemment j'ai pu découvrir React et Api platform
-                        </p>
-                        <p>
-                          Ayant terminé un stage en application mobile pour
-                          Senzu je suis actuellement à lécoute d'oportunité !
-                        </p>
-                      </div>
+                      <p className="underTitle">Je suis développeur web</p>
                     </Fade>
                   </div>
-                </div>
-                {/* End Slide 2 */}
-
-                {/* Slide 3 */}
-                <div className="carouselSlide  myCyan">
-                  <div className={hide2 ? "hide" : ""}>
-                    <Slide top></Slide>
-                    <Fade right>
-                      <div id="SkillBox">
-                        <h3 className="mySkills">Mes compétences</h3>
-
-                        <div className="SkillBar">
-                          <div id="Skill-C">
-                            <span className="Skill-Area ">Symfony</span>
-                            <span className="PercentText ">70%</span>
-                          </div>
+                  {/* End Slide 1 */}
+                  {/* Slide 2 */}
+                  <div className="carouselSlide  myGrey">
+                    <div className="contactMe">
+                      <p>
+                        <i className="fas fa-envelope"></i>
+                        noel.an.lyon@gmail.com
+                      </p>
+                      <p>
+                        <i className="fas fa-phone"></i>06 83 07 54 48
+                      </p>
+                      <a href="https://www.linkedin.com/in/noel-an-704482186/">
+                        <p>
+                          <i className="fab fa-linkedin"></i>Linkedin
+                        </p>
+                      </a>
+                    </div>
+                    <div className={hide1 ? "hide" : ""}>
+                      <Slide top>
+                        <h3 className="myTitleAboutMe">About me</h3>
+                      </Slide>
+                      <Fade right>
+                        <div className="textAboutMe test">
+                          <p>
+                            Bonjour je me présente Noël AN. Après avoir
+                            travaillé principalement dans la restauration et la
+                            logistique, j'ai décidé de me reconvertir dans un
+                            domaine qui m'a toujours passionné, le devéloppement
+                            Web.
+                          </p>
+                          <p>
+                            J'ai effectué une formation de développeur web à la
+                            Wild Code School qui m'a permis de découvrir ce
+                            métier et me spécialiser en PHP Symfony.
+                          </p>
+                          <p>
+                            Je continue de m'auto-former continuellement,
+                            récemment j'ai pu découvrir React et Api platform
+                          </p>
+                          <p>
+                            Ayant terminé un stage en application mobile pour
+                            Senzu je suis actuellement à lécoute d'oportunité !
+                          </p>
                         </div>
-
-                        <div className="SkillBar">
-                          <div id="Skill-XML">
-                            <span className="Skill-Area ">React </span>
-                            <span className="PercentText ">60%</span>
-                          </div>
-                        </div>
-
-                        <div className="SkillBar">
-                          <div id="Skill-JAVA">
-                            <span className="Skill-Area ">Api platform </span>
-                            <span className="PercentText ">50%</span>
-                          </div>
-                        </div>
-
-                        <div className="SkillBar">
-                          <div id="Skill-JS">
-                            <span className="Skill-Area ">Javascript</span>
-                            <span className="PercentText ">65%</span>
-                          </div>
-                        </div>
-
-                        <div className="SkillBar">
-                          <div id="Skill-PHP">
-                            <span className="Skill-Area ">PHP </span>
-                            <span className="PercentText ">70%</span>
-                          </div>
-                        </div>
-
-                        <div className="SkillBar">
-                          <div id="Skill-SQL">
-                            <span className="Skill-Area ">SQL </span>
-                            <span className="PercentText ">80%</span>
-                          </div>
-                        </div>
-
-                        <div className="SkillBar">
-                          <div id="Skill-HTML">
-                            <span className="Skill-Area ">HTML</span>
-                            <span className="PercentText ">99% ?</span>
-                          </div>
-                        </div>
-
-                        <div className="SkillBar">
-                          <div id="Skill-CSS">
-                            <span className="Skill-Area ">CSS/CSS3</span>
-                            <span className="PercentText ">70%</span>
-                          </div>
-                        </div>
-                      </div>
-                    </Fade>
+                      </Fade>
+                    </div>
                   </div>
-                </div>
-                {/* End Slide 3 */}
-              </Slider>
-            </Fade>
+                  {/* End Slide 2 */}
+
+                  {/* Slide 3 */}
+                  <div className="carouselSlide  myCyan">
+                    <div className="contactMe">
+                      <p>
+                        <i className="fas fa-envelope"></i>
+                        noel.an.lyon@gmail.com
+                      </p>
+                      <p>
+                        <i className="fas fa-phone"></i>06 83 07 54 48
+                      </p>
+                      <a href="https://www.linkedin.com/in/noel-an-704482186/">
+                        <p>
+                          <i className="fab fa-linkedin"></i>Linkedin
+                        </p>
+                      </a>
+                    </div>
+                    <div className={hide2 ? "hide" : ""}>
+                      <Slide top></Slide>
+                      <Fade right>
+                        <div id="SkillBox">
+                          <h3 className="mySkills">Mes compétences</h3>
+
+                          <div className="SkillBar">
+                            <div id="Skill-C">
+                              <span className="Skill-Area ">Symfony</span>
+                              <span className="PercentText ">70%</span>
+                            </div>
+                          </div>
+
+                          <div className="SkillBar">
+                            <div id="Skill-XML">
+                              <span className="Skill-Area ">React </span>
+                              <span className="PercentText ">60%</span>
+                            </div>
+                          </div>
+
+                          <div className="SkillBar">
+                            <div id="Skill-JAVA">
+                              <span className="Skill-Area ">Api platform </span>
+                              <span className="PercentText ">50%</span>
+                            </div>
+                          </div>
+
+                          <div className="SkillBar">
+                            <div id="Skill-JS">
+                              <span className="Skill-Area ">Javascript</span>
+                              <span className="PercentText ">65%</span>
+                            </div>
+                          </div>
+
+                          <div className="SkillBar">
+                            <div id="Skill-PHP">
+                              <span className="Skill-Area ">PHP </span>
+                              <span className="PercentText ">70%</span>
+                            </div>
+                          </div>
+
+                          <div className="SkillBar">
+                            <div id="Skill-SQL">
+                              <span className="Skill-Area ">SQL </span>
+                              <span className="PercentText ">80%</span>
+                            </div>
+                          </div>
+
+                          <div className="SkillBar">
+                            <div id="Skill-HTML">
+                              <span className="Skill-Area ">HTML</span>
+                              <span className="PercentText ">99% ?</span>
+                            </div>
+                          </div>
+
+                          <div className="SkillBar">
+                            <div id="Skill-CSS">
+                              <span className="Skill-Area ">CSS/CSS3</span>
+                              <span className="PercentText ">70%</span>
+                            </div>
+                          </div>
+                        </div>
+                      </Fade>
+                    </div>
+                  </div>
+                  {/* End Slide 3 */}
+                </Slider>
+              </Fade>
+            </div>
             <MyProjects></MyProjects>
           </>
         )}
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,69 +1,70 @@
 import React from "react";
-import Fade from "react-reveal/Fade";
 
 const MyProjects = props => {
   // Title, description, img , fade direction
 
   return (
     <>
-      <div class="myProject" id="project">
-        <h1 class="righteous myWhite text-center font-weight-bold p-5">
+      <div className="myProject" id="project">
+        <h1 className="righteous myWhite text-center font-weight-bold p-5">
           Expériences et projets
         </h1>
-        <div class="container">
-          <div class="row justify-content-center ">
-            <div class="col-lg-8 col-sm-12">
-              <h3 class="aPropos text-center">Stage chez Senzu ( 1 mois )</h3>
+        <div className="container">
+          <div className="row justify-content-center ">
+            <div className="col-lg-8 col-sm-12">
+              <h3 className="aPropos text-center">
+                Stage chez Senzu ( 1 mois )
+              </h3>
               <p>
                 Création d'une application Android permettant d'utiliser les
                 données des puces NFC.
               </p>
               <p>Prise en main du logiciel Android studio</p>
-              <div class="text-center">
-                <p class="btn btn-primary">Android Studio</p>
-                <p class="btn btn-secondary">Java</p>
+              <div className="text-center">
+                <p className="btn btn-primary">Android Studio</p>
+                <p className="btn btn-secondary">Java</p>
               </div>
             </div>
-            <div class="col-lg-4 col-sm-12">
+            <div className="col-lg-4 col-sm-12">
               <img
                 alt="img"
-                class="myImg"
+                className="myImg"
                 src="https://images.pexels.com/photos/226664/pexels-photo-226664.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
               />
             </div>
           </div>
           <hr />
-          <div class="row justify-content-center">
-            <div class="col-lg-8 col-sm-12">
-              <h3 class="aPropos text-center">Tutoriels book !</h3>
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-sm-12">
+              <h3 className="aPropos text-center">Tutoriels book !</h3>
               <p>Site 'bac à sable' regroupant des tutoriels vidéos !</p>
               <p>
                 Mise en place d'une API (Api Platform) et un rendu en React
                 (Javascript)
               </p>
-              <div class="text-center">
-                <p class="btn btn-primary">Smyfony</p>
-                <p class="btn btn-secondary">Api Platform</p>
-                <p class="btn btn-warning">React</p>
+              <div className="text-center">
+                <p className="btn btn-primary">Smyfony</p>
+                <p className="btn btn-secondary">Api Platform</p>
+                <p className="btn btn-warning">React</p>
                 <a href="https://tutoriels-book.herokuapp.com/#/">
-                  <p class="text-center">Celui-ci est en ligne !</p>
+                  <p className="text-center">Celui-ci est en ligne !</p>
                 </a>
               </div>
             </div>
-            <div class="col-lg-4 col-sm-12">
+            <div className="col-lg-4 col-sm-12">
               <a href="https://tutoriels-book.herokuapp.com/#/">
                 <img
                   alt="img"
-                  class="myImg"
+                  className="myImg"
                   src="https://zupimages.net/up/20/02/dkmd.png"
                 />
               </a>
             </div>
           </div>
           <hr />
-          <div class="row justify-content-center">
-            <div class="col-lg-8 col-sm-12">
-              <h3 class="aPropos text-center">Funny Shop</h3>
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-sm-12">
+              <h3 className="aPropos text-center">Funny Shop</h3>
               <p>
                 Site de E commerce 'bac à sable' créé pour parfaire mes
                 compétences et principalement avec le framework Symfony.
@@ -74,28 +75,28 @@ const MyProjects = props => {
                 en constante évolution !
               </p>
               <a href="https://cloth-a.herokuapp.com/">
-                <p class="text-center">Celui-ci est en ligne !</p>
+                <p className="text-center">Celui-ci est en ligne !</p>
               </a>
-              <div class="text-center">
-                <p class="btn btn-primary">Symfony</p>
-                <p class="btn btn-success">Twig</p>
-                <p class="btn btn-info">Doctrine</p>
+              <div className="text-center">
+                <p className="btn btn-primary">Symfony</p>
+                <p className="btn btn-success">Twig</p>
+                <p className="btn btn-info">Doctrine</p>
               </div>
             </div>
-            <div class="col-lg-4 col-sm-12">
+            <div className="col-lg-4 col-sm-12">
               <a href="https://cloth-a.herokuapp.com/">
                 <img
                   alt="img"
-                  class="myImg"
+                  className="myImg"
                   src="https://zupimages.net/up/20/02/nx8t.png"
                 />
               </a>
             </div>
           </div>
           <hr />
-          <div class="row justify-content-center">
-            <div class="col-lg-8 col-sm-12">
-              <h3 class="aPropos text-center">Event Team</h3>
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-sm-12">
+              <h3 className="aPropos text-center">Event Team</h3>
               <p>
                 Site réalisé en cours de formation permettant de gerer le
                 planning des utilisateur 'Google Agenda like'.
@@ -104,25 +105,25 @@ const MyProjects = props => {
                 Mise en place du Modèle Vue Controlleur, travail en équipe (4
                 personnes)en méthode agile.
               </p>
-              <div class="text-center">
-                <p class="btn btn-primary">PHP</p>
-                <p class="btn btn-success">Scrum</p>
-                <p class="btn btn-secondary">Git</p>
-                <p class="btn btn-warning">Js Vanilla</p>
+              <div className="text-center">
+                <p className="btn btn-primary">PHP</p>
+                <p className="btn btn-success">Scrum</p>
+                <p className="btn btn-secondary">Git</p>
+                <p className="btn btn-warning">Js Vanilla</p>
               </div>
             </div>
-            <div class="col-lg-4 col-sm-12">
+            <div className="col-lg-4 col-sm-12">
               <img
                 alt="img"
-                class="myImg"
+                className="myImg"
                 src="https://images.pexels.com/photos/3299/postit-scrabble-to-do.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
               />
             </div>
           </div>
           <hr />
-          <div class="row justify-content-center">
-            <div class="col-lg-8 col-sm-12">
-              <h3 class="aPropos text-center">Site de solfège débutant</h3>
+          <div className="row justify-content-center">
+            <div className="col-lg-8 col-sm-12">
+              <h3 className="aPropos text-center">Site de solfège débutant</h3>
               <p>
                 Site réalisé en parallèle de la formation permettant d'apprendre
                 le solfège à travers des mini-jeux.
@@ -131,14 +132,14 @@ const MyProjects = props => {
                 Ce projet a été réalisé afin découvrir de et approfondir le
                 Javascript Vanilla
               </p>
-              <div class="text-center">
-                <p class="btn btn-warning">Javascript Vanilla</p>
+              <div className="text-center">
+                <p className="btn btn-warning">Javascript Vanilla</p>
               </div>
             </div>
-            <div class="col-lg-4 col-sm-12">
+            <div className="col-lg-4 col-sm-12">
               <img
                 alt="img"
-                class="myImg"
+                className="myImg"
                 src="https://images.pexels.com/photos/1210530/pexels-photo-1210530.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
               />
             </div>
